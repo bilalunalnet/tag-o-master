00612384d153c3700a972d5fafd9e80d5602d381
Bilal Ünal <admin@bilalunal.net>
2017-03-05 17:43:11 +0300

    added ajax feature

--- js/tom-script.js
+++ js/tom-script.js
@@ -2,3 +2,32 @@
  * Created by bilalunalnet on 2.03.2017.
  */
 
+jQuery(document).ready(function () {
+   jQuery('.post-like a').click(function () {
+       var button = jQuery(this);
+       var post_id = button.data('post_id');
+       var event = button.data('event');
+       if (event == 'like') {
+           button.text('dislike');
+           button.data('event','unlike');
+           button.attr('class', 'unlike');
+       }else {
+           button.text('like');
+           button.data('event','like');
+           button.attr('class', 'like');
+       }
+       jQuery.ajax({
+           type : 'post',
+           url : tom_ajax.ajax_url,
+           data : {
+               action : 'like',
+               post_id : post_id,
+               event : event,
+               nonce : tom_ajax.nonce
+           },
+           success : function (response) {
+               jQuery('.count').text(response);
+           }
+       });
+   });
+});
--- tag-o-master.php
+++ tag-o-master.php
@@ -16,34 +16,35 @@ class tagOmaster {
      * Action hooks
      */
     public function run() {
-        // Enqueue plugin styles and scripts
-        add_action('plugins_loaded', array($this,'enqueue_tom_scripts'));
-        add_action('plugins_loaded', array($this,'enqueue_tom_styles'));
+        // Register-Enqueue plugin styles and scripts
+        add_action('init', array($this,'register_script'));
+        add_action('wp_enqueue_scripts', array($this,'enqueue_style'));
         // Setup filter hook to place button under the each post
-        add_filter( 'the_content', array( $this, 'like_button' ) );
-    }
-
-    /**
-     * Register plugin styles and scripts
-     */
-    public function register_tom_scripts() {
-        wp_register_script( 'tom-script', plugins_url( 'js/tom-script.js', __FILE__ ), array('jquery'), null, true );
-        wp_register_style( 'tom-style', plugins_url( 'css/tagomaster_main.css' ) );
+        add_filter('the_content', array($this, 'like_button' ));
+        // Add ajax custom hook
+        add_action('wp_ajax_nopriv_like', array($this,'like'));
+        add_action('wp_ajax_like', array($this,'like'));
     }
     /**
-     * Enqueues plugin-specific scripts.
+     * register jquery and style on initialization
      */
-    public function enqueue_tom_scripts() {
-        wp_enqueue_script( 'tom-script' );
-        wp_localize_script( 'tom-script', 'tom_ajax', array( 'ajax_url' => admin_url('admin-ajax.php')) );
+    public function register_script() {
+        wp_register_script('tom-script', plugins_url('js/tom-script.js', __FILE__), array('jquery'), '2.5.1' );
+        wp_register_style('tom-style', plugins_url('css/tagomaster_main.css', __FILE__), false, '1.0.0', 'all');
+        wp_localize_script('tom-script', 'tom_ajax',
+            array(
+                'ajax_url' => admin_url('admin-ajax.php'),
+                'nonce' => wp_create_nonce('i-just-need-a-coffee')
+            )
+        );
     }
     /**
-     * Enqueues plugin-specific styles.
+     * use the registered jquery and style above
      */
-    public function enqueue_tom_styles() {
-        wp_enqueue_style( 'tom-style' );
+    public function enqueue_style(){
+        wp_enqueue_script('tom-script');
+        wp_enqueue_style('tom-style');
     }
-
     /**
      * Adding like button to end of post
      * @param $content
@@ -51,11 +52,62 @@ class tagOmaster {
      */
     public function like_button($content) {
         if (get_post_type() == post && is_singular()) {
-            $html = '<a href="#" class="tom-likebtn" data-id="' . get_the_ID() . '">Like</a>';
+            $html = '<p class="post-like"><a data-event="like" data-post_id="'.get_the_ID().'" href="#">';
+            $html .= 'Like</a><span class="count">'.$this->likesCount(get_the_ID()).'</span></p>';
             $content .= $html;
         }
         return $content;
     }
+    /**
+     * AJAX response function
+     */
+    public function like () {
+        check_ajax_referer('i-just-need-a-coffee', 'nonce');
+        $post_id = $_POST['post_id'];
+        $event = $_POST['event'];
+        if ($event == "like") {
+            $this->likePost($post_id);
+        }else{
+            $this->dislikePost($post_id);
+        }
+        die();
+    }
+    /**
+     * @param $post_id
+     * helper function to like post
+     */
+    public function likePost($post_id) {
+        $likes_count = $this->likesCount($post_id);
+        if ($likes_count) {
+            update_post_meta($post_id, '_likes_count', ++$likes_count);
+            echo $likes_count;
+        }else{
+            add_post_meta($post_id, '_likes_count', 1);
+            echo 1;
+        }
+    }
+    /**
+     * @param $post_id
+     * helper function to dislike post
+     */
+    public function dislikePost($post_id) {
+        $likes_count = $this->likesCount($post_id);
+        if ($likes_count) {
+            update_post_meta($post_id, '_likes_count', --$likes_count);
+            echo $likes_count;
+        }else{
+            add_post_meta($post_id, '_likes_count', -1);
+            echo -1;
+        }
+    }
+    public function likesCount($post_id) {
+        return get_post_meta($post_id,'_likes_count',true);
+    }
+
+    public function hasAlreadyLiked($post_id) {
+
+    }
+
 }
 
 $plugin = new tagOmaster();
